ac1123b1f69015885a45bdf221562a2dd7d00cc2
OdinTakoy <bugazov9@mail.ru>
2022-09-14 11:19:46 +0300

    сделал стейты

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -9,14 +9,16 @@ import Movie from "../components/Movie";
 import styles from "../components/styles/main.module.css";
 import { Link, useParams } from "react-router-dom";
 const MainPages = () => {
+  const [active,setActive] = useState(0)
   const dispatch = useDispatch();
+  const categories = ["ВСЕ","Платные","Бесплатные"]
   const years = [2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009]
   const country = ["Россия","США","Великобритания","Франция", "Испания"]
   const params = useParams();
   const id = params.id
-  const yearid = params.cat
-
-
+  const [titleGenre,settitleGenre] = useState("Все жанры")
+  const [titleYears,settitleYears] = useState("Все года")
+  const [titleCountry,settitleCountry] = useState("Все страны")
 
   useEffect(() => {
     dispatch(fetchmovies());
@@ -26,6 +28,19 @@ const MainPages = () => {
   const genres = useSelector((state) => state.genres.genres);
   const movies = useSelector((state) => state.movies.movies);
 
+  const handleClickGenre= (name)=>{
+settitleGenre(name)
+  }
+  const handleClickYears= (name)=>{
+    settitleYears(name)
+      }
+      const handleClickCountry= (name)=>{
+        settitleCountry(name)
+          }
+
+  const onclickCategory = (index)=>{
+    setActive(index)
+  }
 
   const filteredgenre = movies.filter((item)=>{
     console.log(item.year)
@@ -43,35 +58,35 @@ const MainPages = () => {
       </div>
       <div className={styles.categoryfilters}>
         <div className={styles.categoryfilters_item}>
-          <div>Все</div>
-          <div>Бесплатные</div>
-          <div>Платные</div>
+          {categories.map((item,i)=>{
+            return <div key={i} onClick={() => onclickCategory(i)} className={active === i ? styles.categoryActive : styles.categorynoActive}>{item}</div>
+          })}
         </div>
         <div className={styles.dropdown}>
-          <button className={styles.dropbtn}>Все Жанры</button>
+          <button className={styles.dropbtn}>{titleGenre}</button>
           <ul className={styles.dropdown_content} name="Жанры">
             {genres.map((genre) => {
               return (
                 <li>
-                  <Link to={`/genre/${genre._id}`}> {genre.nameGenre}</Link>
+                  <Link onClick={()=> handleClickGenre(genre.nameGenre)} to={`/genre/${genre._id}`}> {genre.nameGenre}</Link>
                 </li>
               );
             })}
           </ul>
         </div>
         <div className={styles.dropdown}>
-        <button className={styles.dropbtn}>Все года</button>
+        <button className={styles.dropbtn}>{titleYears}</button>
           <ul className={styles.dropdown_content}>
             {years.map((i)=>{
-              return <li><Link to={`/years/${i}`}>{i}</Link></li>
+              return <li><Link onClick={()=> handleClickYears(i)} to={`/years/${i}`}>{i}</Link></li>
             })}
           </ul>
         </div>
         <div className={styles.dropdown}>
-        <button className={styles.dropbtn}>Все года</button>
+        <button className={styles.dropbtn}>{titleCountry}</button>
           <ul className={styles.dropdown_content}>
             {country.map((i)=>{
-              return <li><Link to={`/country/${i}`}>{i}</Link></li>
+              return <li><Link onClick={()=> handleClickCountry(i)} to={`/country/${i}`}>{i}</Link></li>
             })}
           </ul>
         </div>
