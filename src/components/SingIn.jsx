406777638e4a8b5f3a96d346675ab9c55f64156f
”Ismail” <ibeno95@mail.ru>
2022-09-17 22:26:31 +0300

    сделал стили для страницы авторизации

--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { loginThunk } from "../features/applicationSlice";
+import styles from "./styles/authPage.module.css";
 
 const SingIn = () => {
-  const token = useSelector((state)=> state.application.token)
+  const token = useSelector((state) => state.application.token);
   const dispatch = useDispatch();
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
@@ -18,24 +20,34 @@ const SingIn = () => {
     setLogin("");
     setPassword("");
     dispatch(loginThunk({ login, password }));
-  
   };
 
   return (
-    <div>
+    <div className={styles.auth}>
+      <h2 className={styles.auth_title}>Авторизация</h2>
       <input
+        className={styles.auth_input}
         placeholder="ЛОГИН"
         onChange={(e) => handleChange(e)}
         value={login}
         type="text"
       />
       <input
+        className={styles.auth_input}
         placeholder="ПАРОЛЬ"
         onChange={(e) => handleChangePas(e)}
         value={password}
         type="text"
       />
-      <button onClick={() => handleClick()}>"Войти"</button>
+      <p>У вас нет аккаунта? </p>
+
+      <p className={styles.link}>
+        <Link to="/auth">Зарегистрируйтесь</Link>
+      </p>
+
+      <button className={styles.auth_btn} onClick={() => handleClick()}>
+        Войти
+      </button>
     </div>
   );
 };
